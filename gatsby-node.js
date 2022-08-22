c4fa44387b58bf5ff4b725162beff2bd048bb06f
황수영 <blythe@jstorm.kr>
2022-08-22 13:43:20 +0900

    feat: 상세 페이지 연결
    - Slug
    - 상세 페이지 : templates components
    - 상세 조회

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,66 @@
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql, reporter }) => {
     const { createPage } = actions
-    createPage({
-        path: '/using-dsg',
-        component: path.resolve('./src/pages/index.tsx'),
-        context: {},
-        defer: true,
-    })
+    // createPage({
+    //     path: '/using-dsg',
+    //     component: path.resolve('./src/pages/index.tsx'),
+    //     context: {},
+    //     defer: true,
+    // })
+
+    // Get All Markdown File For Paging
+    const queryAllMarkdownData = await graphql(
+        `
+            {
+                allMarkdownRemark(
+                    sort: {
+                        order: DESC
+                        fields: [frontmatter___date, frontmatter___title]
+                    }
+                ) {
+                    edges {
+                        node {
+                            fields {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+    )
+
+    // Handling GraphQL Query Error
+    if (queryAllMarkdownData.errors) {
+        reporter.panicOnBuild(`Error while running query`)
+        return
+    }
+
+    // Import Post Template Component
+    const PostTemplateComponent = path.resolve(
+        __dirname,
+        'src/templates/post_template.tsx',
+    )
+
+    // Page Generating Function
+    const generatePostPage = ({
+        node: {
+            fields: { slug },
+        },
+    }) => {
+        const pageOptions = {
+            path: slug,
+            component: PostTemplateComponent,
+            context: { slug },
+        }
+
+        createPage(pageOptions)
+    }
+
+    // Generate Post Page And Passing Slug Props for Query
+    queryAllMarkdownData.data.allMarkdownRemark.edges.forEach(generatePostPage)
 }
 
 /**
@@ -32,4 +85,15 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
             },
         },
     })
+}
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode })
+
+        createNodeField({ node, name: 'slug', value: slug })
+    }
 }
